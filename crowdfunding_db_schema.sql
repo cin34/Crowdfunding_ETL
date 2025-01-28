c863fbb991f382571a8b839659f420830e762f02
cin34 <cin122589@hotmail.com>
2025-01-27 19:35:48 -0600

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -22,23 +22,23 @@ SELECT * FROM subcategory;
 
 
 CREATE TABLE campaign (
-    cf_id INT PRIMARY KEY NOT NULL,
-    contact_id INT   NOT NULL,
-	company_name VARCHAR(50)   NOT NULL,
-	description  VARCHAR(255)   NOT NULL,
-	goal FLOAT   NOT NULL,
-	pledge FLOAT   NOT NULL,
-	outcome  VARCHAR(50)   NOT NULL,
-    backers_count  INT   NOT NULL,
-    country VARCHAR(50)   NOT NULL,
-    currency VARCHAR(50)   NOT NULL,
-	launch_date DATE   NOT NULL,
-	end_date DATE   NOT NULL,
-    category_id VARCHAR(10)   NOT NULL,
-    subcategory_id VARCHAR(10)   NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(category_id),
-    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
-    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
+   cf_id INT PRIMARY KEY NOT NULL,
+   contact_id INT   NOT NULL,
+   company_name VARCHAR(50)   NOT NULL,
+   description  VARCHAR(255)   NOT NULL,
+   goal FLOAT   NOT NULL,
+   pledge FLOAT   NOT NULL,
+   outcome  VARCHAR(50)   NOT NULL,
+   backers_count  INT   NOT NULL,
+   country VARCHAR(50)   NOT NULL,
+   currency VARCHAR(50)   NOT NULL,
+   launch_date DATE   NOT NULL,
+   end_date DATE   NOT NULL,
+   category_id VARCHAR(10)   NOT NULL,
+   subcategory_id VARCHAR(10)   NOT NULL,
+   FOREIGN KEY (category_id) REFERENCES category(category_id),
+   FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id),
+   FOREIGN KEY (contact_id) REFERENCES contacts(contact_id)
 );
 
 SELECT * FROM campaign;
